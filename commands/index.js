a95378112055a5fa62e931875c916c848251391b
Knut Kirkhorn <knutkirk@hotmail.com>
2025-05-29 14:57:06 +0200

    Ensure user config are setup

--- commands/index.js
+++ commands/index.js
@@ -1,6 +1,49 @@
+import fs from 'node:fs/promises';
+import os from 'node:os';
+import path from 'node:path';
+import {fileExists} from '../util.js';
 import {addEditorConfig} from './add/editorconfig.js';
 import {addEslint} from './add/eslint.js';
 import {addPrettier} from './add/prettier.js';
+
+async function ensureUserConfigDirectoriesExists() {
+	const homeDirectory = os.homedir();
+	const configDirectories = ['eslint', 'prettier', 'editorconfig'];
+
+	for (const directory of configDirectories) {
+		const configDirectoryPath = path.join(
+			homeDirectory,
+			'.sofic',
+			'configs',
+			directory,
+		);
+		await fs.mkdir(configDirectoryPath, {recursive: true});
+	}
+}
+
+async function ensureUserConfigFileExists() {
+	const homeDirectory = os.homedir();
+	const configFile = path.join(
+		homeDirectory,
+		'.sofic',
+		'configs',
+		'configs.json',
+	);
+
+	if (await fileExists(configFile)) {
+		return;
+	}
+
+	// Use version from the current package.json
+	// The version will be for future potential breaking changes
+	const packageJson = JSON.parse(await fs.readFile('./package.json', 'utf8'));
+	const config = {
+		version: packageJson.version,
+		configs: {},
+	};
+	// Save the config
+	await fs.writeFile(configFile, JSON.stringify(config, undefined, 2));
+}
 
 export async function add(tool, flags) {
 	if (flags.list || !tool) {
@@ -10,6 +53,10 @@ export async function add(tool, flags) {
 		console.log('  - editorconfig');
 		return;
 	}
+
+	// Ensure the user config are setup
+	await ensureUserConfigDirectoriesExists();
+	await ensureUserConfigFileExists();
 
 	// Run the add command for the tool
 	switch (tool) {
