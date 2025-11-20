8f46b02d87c2a52fb9831832c194092e052115c0
Knut Kirkhorn <knutkirk@hotmail.com>
2025-11-20 22:05:09 +0100

    Test fix

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -2,6 +2,6 @@ import {expect, test} from 'bun:test';
 import {execa} from 'execa';
 
 test('cli can print out', async () => {
-	const {stdout} = await execa('node', ['./dist/cli.js', '--version']);
+	const {stdout} = await execa('node', ['dist/cli.js', '--version']);
 	expect(stdout.length).toBeGreaterThan(0);
 });
